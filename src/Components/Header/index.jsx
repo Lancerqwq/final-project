e77e826f1e38d356b5ca1a24bd9a0d3065297940
Dima <voronoydima@gmail.com>
2021-07-12 00:17:07 +0300

    chore: merge

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Link as MuiLink, Toolbar } from "@material-ui/core";
+import { AppBar, Link as Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link, NavLink } from "react-router-dom";
 
--- src/Components/Routes/index.jsx
+++ src/Components/Routes/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function Routes() {
   return (
