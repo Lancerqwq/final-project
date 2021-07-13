afcfc798eb6175ff1a8f96d2c047ad9266a94795
Dima <voronoydima@gmail.com>
2021-07-13 13:56:29 +0300

    chore: removed method call getReauest

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { HTTPService } from "./api/HTTPService";
 import Layout from "./Layout/Layout";
 import Routes from "./Components/Routes/index";
 
 function App() {
-  const [books, setBooks] = useState([]);
-  useEffect(() => {
-    HTTPService.getRequest("todos/20").then((data) => setBooks(data));
-  }, []);
-
   return (
     <div className="App">
       <Router>
